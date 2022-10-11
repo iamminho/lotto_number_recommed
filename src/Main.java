73b4ab9c47e79a1c2abc0e68702117e613ada4d9
iamminho <minho100227@kakao.com>
2022-10-11 19:14:08 +0900

    func lotto number recommend

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random random = new Random();
+
+        System.out.print("예상 로또 번호 : ");
+        for (int i = 0; i < 6; i++) {
+            System.out.print("[" + (random.nextInt(45) + 1) + "]" + " ");
+        }
     }
 }
